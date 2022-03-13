3014d620e88c7959a87f69e683ce09e229ac7b89
minte9 <catalinprescure@gmail.com>
2022-03-13 17:05:04 +0200

    spring-boot update

---
+++ quick_start/src/main/java/com/minte9/demo/DemoApplication.java
@@ -0,0 +1,36 @@
+/**
+ * spring-boot/demo//src/main/java/com/example/DemoApplication.java 
+ * 
+ * Spring Boot is the most popular way to start Spring projects.
+ * Spring Boot is built on top of the spring framework.
+ *
+ * Start by creating a new Spring Web project ...
+ * by opening intializer on start.spring.io
+ */
+
+package com.minte9.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+}
