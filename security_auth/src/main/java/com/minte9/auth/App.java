4c6ed2a2375c0112d74cbd259e20159be9596f25
minte9 <catalinprescure@gmail.com>
2023-02-27 19:31:40 +0200

    spring-boot update

--- security_auth/src/main/java/com/minte9/auth/App.java
+++ security_auth/src/main/java/com/minte9/auth/App.java
@@ -1,6 +1,7 @@
 /**
  * Spring Boot Application
  */
+
 package com.minte9.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,5 +12,4 @@ public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
-
 }
