7e891578575880243d6ec376da6d9f5ccd5f74b3
minte9 <catalinprescure@gmail.com>
2023-02-27 19:18:56 +0200

    spring-boot update

---
+++ security_auth/src/main/java/com/minte9/auth/MvcConfig.java
@@ -0,0 +1,18 @@
+/**
+ * App Configuration
+ */
+
+package com.minte9.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("hello");
+        registry.addViewController("/hello").setViewName("hello");
+    }
+}
