789ab60e0c5dccb28b85e4b27b1f5a2d550e7d2d
minte9 <catalinprescure@gmail.com>
2023-02-28 10:48:50 +0200

    spring-boot update

---
+++ command_line/hello/App.groovy
@@ -0,0 +1,16 @@
+/**
+ * Standard Groovy includes a Grab annotation ...
+ * to declare dependencies.
+ * 
+ * With RestController annotation ... 
+ * Spring Boot grabs Tomcat and Spring Mvc.
+ */
+
+@RestController
+class App {
+
+    @RequestMapping("/") 
+    String home() {
+        "Hello Groovy"
+    }
+}
