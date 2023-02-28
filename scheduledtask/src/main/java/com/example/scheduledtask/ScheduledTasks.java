0dd1db9fb4f6ec5c70d11c35867a1a2f74a7c949
minte9 <catalinprescure@gmail.com>
2023-02-28 12:58:15 +0200

    spring-boot update

---
+++ scheduledtask/src/main/java/com/example/scheduledtask/ScheduledTasks.java
@@ -0,0 +1,19 @@
+package com.example.scheduletask;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component // Look Here
+public class ScheduledTasks {
+    
+    private static final SimpleDateFormat dateFormat = 
+        new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 1000) // 1 second
+    public void runningTime() {
+        System.out.println(
+            "Time is: " + dateFormat.format(new Date())
+        );
+    }
+}
