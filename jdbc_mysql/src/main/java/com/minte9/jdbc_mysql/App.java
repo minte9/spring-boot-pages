18e6c799b5fdcd9df92f825a4da6ebe0d3eff417
minte9 <catalinprescure@gmail.com>
2023-03-01 11:38:47 +0200

    spring-boot update

--- jdbc_mysql/src/main/java/com/minte9/jdbc_mysql/App.java
+++ jdbc_mysql/src/main/java/com/minte9/jdbc_mysql/App.java
@@ -21,11 +21,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
