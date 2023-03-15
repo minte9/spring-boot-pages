9c0c8900621949f3d137876f8a46a3c1788822a1
minte9 <catalinprescure@gmail.com>
2023-03-15 16:10:14 +0200

    spring-boot update

--- consuming_rest/README.md
+++ consuming_rest/README.md
@@ -2,32 +2,31 @@
 
 ## Service API
 
-Run service api `rest_service` in a separate terminal on port 9090.
+Run `rest_service` in a separate terminal on port 9090.
 
-#--
+~~~
 cd rest_service
 mvn spring-boot:run # start service
 
 http://localhost:9090
-http://localhost:8080/getquote/1
-http://localhost:8080/getquote/2
---#
+  // Welcome! Use /quote/{id}
+~~~
 
 ### Client App
 
-The application will retrive `quotes` from a RESTful web service.
+The application will retrive `quotes` from the REST web service.
 
 ~~~
 http://localhost:9090/getquote/1
-# {"type":"success","value":{"id":7,"quote":"The real benefit of Boot, ...
+  // {"id":1,"content":"Spring Boot makes stand alone Spring apps easy."}
 
 http://localhost:9090/getquote/2
-# Quote{type='success', value=Value{id='7', quote=The real benefit of Boot, ...
+  // {"id":2,"content":"With Boot you deploy everywhere."}
 ~~~
 
 ### Run client
 
-Start a new `Spring Boot Web` project on port 8080
+Start a new Spring Boot `Web` project on port 8080
 
 ~~~ini
 # /src/main/resources/application.properties
@@ -47,16 +46,9 @@ Change maven `compiler version`, if needed.
 
 ### Main
 
-Application class with `mapping` /getquote
+Application class with `/request_quote` mapping.
 
 #-- consuming_rest/App.java --#
-
-### Model
-
-Domain classes, `Quote` and Value.
-
-#-- consuming_rest/Quote.java --#
-#-- consuming_rest/Value.java --#
 
 
 ### Test
@@ -67,12 +59,9 @@ Build and run the application in `console` or browser.
 cd consuming_rest/
 mvn spring-boot:run
 
-# browser
-  http://localhost:9090/getquote
+http://localhost:9090/getquote
+  // {"id":1,"content":"Spring Boot makes stand alone Spring apps easy."}
 
-# console
-  java -jar target/democonsumingrest-0.0.1.jar 
-
-# result
-Quote{type='success', value=Value{id='9', quote='Spring Boot solves ...'}
+java -jar target/democonsumingrest-0.0.1.jar 
+  // {"id":2,"content":"With Boot you deploy everywhere."}
 ~~~
---
+++ consuming_rest/src/main/java/com/minte9/consuming_rest/App.java
@@ -0,0 +1,64 @@
+/**
+ * Consuming REST App
+ * 
+ * Add mapping (/getquote) from a separate service on 8080 port
+ * Use a domain class (Quote) to contain the data that you need.
+ * Any property not bound will be ignored.
+ * 
+ * RestTemplate will use the Jackson JSON library for incomming data.
+ * CommandLineRunner will run the RestTemplate on startup.
+ */
+
+package com.minte9.consuming_rest;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "REST Api Client <br><br>Use /request_quote";
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @GetMapping("/request_quote")
+    public Object quote() {
+        RestTemplate rt = new RestTemplate(); // Browser
+
+        Object quote = rt.getForObject(
+            "http://localhost:9090/quote/1", Object.class
+        );
+        return quote;
+            // {"id":1,"content":"Spring Boot makes stand alone Spring apps easy."}
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) // Console (from target/)
+        throws Exception {
+
+            return args -> {
+                Object quote = restTemplate.getForObject(
+                    "http://localhost:9090/quote/2", Object.class
+                );
+
+                System.out.println(quote);
+                    // {"id":2,"content":"With Boot you deploy everywhere."}
+            };
+    }
+}
