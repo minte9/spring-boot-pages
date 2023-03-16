36ca19357138e59096418faf89930396ae68b586
minte9 <catalinprescure@gmail.com>
2023-03-16 11:09:44 +0200

    spring-boot update

--- templates/src/main/java/com/minte9/templates/HelloController.java
+++ templates/src/main/java/com/minte9/templates/HelloController.java
@@ -9,13 +9,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class GreetingController {
+public class HelloController {
 
     @GetMapping("/")
     public String home() {
         return "index.html";
+    }
+
+    @GetMapping("/message")
+    @ResponseBody
+    public String message() {
+        return "My message"; // just String
     }
     
     /**
