a9d5cdad03ec14de2f8dae6a31b0589118b4848d
minte9 <catalinprescure@gmail.com>
2023-03-20 14:23:32 +0200

    spring-boot update

--- jwt/hello.sh
+++ jwt/hello.sh
@@ -1,8 +1,13 @@
+curl http://localhost:8080/hello
+printf '\n\n'
+    # Access denied
+
 URL='http://localhost:8080/token?user=myuser&password=mypass'
 RESPONSE=$(curl -s -X POST -H "Content-Type: application/json" $URL)
-
 TOKEN=$(echo $RESPONSE | jq -r '.token')
+
 echo $TOKEN
+printf '\n'
     # Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzb2Z0dG...
 
 curl -H "Authorization: Bearer $TOKEN" http://localhost:8080/hello
