87aff341f337b6e9d0ef2db84073b96790735134
minte9 <catalinprescure@gmail.com>
2023-03-20 09:12:55 +0200

    spring-boot update

---
+++ jwt/src/main/java/com/minte9/jwt/AppRestController.java
@@ -0,0 +1,66 @@
+package com.minte9.jwt;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@RestController
+public class AppRestController {
+
+	@ Autowired
+	private Environment env;
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello World! \n";
+	}
+
+	@PostMapping("/token")
+	public User genToken(@RequestParam("user") String username, @RequestParam("password") String pwd) {
+		
+		User user = new User();	
+		user.setUser(username);
+
+		String envUser = env.getProperty("spring.security.user.name");
+		String envPass = env.getProperty("spring.security.user.password");
+
+		if(username.equals(envUser) && pwd.equals(envPass)) {
+			String token = getJWTToken(username);
+			user.setToken(token);
+		}
+
+		return user;
+		
+	}
+
+	private String getJWTToken(String username) {
+		String secretKey = "myTokenSecretKey";
+		List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+				.commaSeparatedStringToAuthorityList("ROLE_USER");
+		
+		String token = Jwts
+				.builder()
+				.setId("softtekJWT")
+				.setSubject(username)
+				.claim("authorities", grantedAuthorities.stream()
+						.map(GrantedAuthority::getAuthority)
+						.collect(Collectors.toList()))
+				.setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + 600000))
+				.signWith(SignatureAlgorithm.HS512, secretKey.getBytes()).compact();
+
+		return "Bearer " + token;
+	}
+}
