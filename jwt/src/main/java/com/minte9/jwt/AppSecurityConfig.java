67c5a2be6a57619a020aa2f4e2edef407b421962
minte9 <catalinprescure@gmail.com>
2023-03-20 12:37:12 +0200

    spring-boot update

--- jwt/src/main/java/com/minte9/jwt/AppSecurityConfig.java
+++ jwt/src/main/java/com/minte9/jwt/AppSecurityConfig.java
@@ -7,9 +7,8 @@ package com.minte9.jwt;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.web.authentication.*;
 
 @EnableWebSecurity
 @Configuration
@@ -19,7 +18,9 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .addFilterAfter(
-                new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                new JWTAuthorizationFilter(), 
+                UsernamePasswordAuthenticationFilter.class
+            )
             .authorizeRequests()
             .antMatchers(HttpMethod.POST, "/token").permitAll()
             .anyRequest().authenticated();
