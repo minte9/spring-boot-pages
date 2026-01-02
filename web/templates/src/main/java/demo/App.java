e90241c5e9f558c621b682d6e7112fbf9ca10218
minte9 <catalinprescure@gmail.com>
2026-01-02 18:23:24 +0200

    spring-boot-pages update

---
+++ web/templates/src/main/java/demo/App.java
@@ -0,0 +1,40 @@
+/**
+ * SPRING BOOT - MVC APPLICATION (TEMPLATES)
+ * -----------------------------------------
+ * This is a Spring Boot MVC application that renders HTML pages
+ * using templates instead of return raw data.
+ * 
+ * Spring Boot + MVC:
+ * ------------------
+ * 	- Spring Boot auto-configures Spring MVC
+ *  - A built-in web server (Tomcat) is started automatically
+ *  - HTML templates are resolved by convention
+ * 
+ * Project SETUP:
+ * --------------
+ * Create the project using:
+ * 		https://start.spring.io
+ * 
+ * Required DEPENDENCIES:
+ * ----------------------
+ * 	- Spring Web 			= MVC + embeded server
+ * 	- Thymealf 				= Template engine for HTML
+ * 	- Spring Boot DevTools	= Auto-restart & live reload
+ * 
+ * CONVENTION over configuration:
+ * ------------------------------
+ * - Templates are placed in:
+ *		src/main/resources/templates
+ */
+
+package demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
---
+++ web/templates/src/main/java/demo/HelloController.java
@@ -0,0 +1,68 @@
+/**
+ * MVC APP - CONTROLLER
+ * --------------------
+ * A controlelr handles HTTP requests and return:
+ *  - A view name (HTML template)
+ *  - OR raw data (if ResponseBody is used)
+ * 
+ * IMPORTANT:
+ * ----------
+ *  - Use Controller for templates
+ *  - Use RestController for APIs
+ *  - Templates do NOT work with RestController
+ * 
+ * Spring Boot DevTools:
+ * ---------------------
+ *  - Automatically reloads changes
+ *  - No server restart needed
+ * 
+ * Test URLs:
+ * ----------
+ * http://localhost:9090
+ * http://localhost:9090/hello
+ * http://localhost:9090/hello/?username=John
+ */
+
+package demo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    /**
+     * Returns the homepage template.
+     */
+    @GetMapping("/")
+    public String home() {
+        return "index.html";
+    }
+
+    /**
+     * Returns raw text instead of a template.
+     * This behaves like a REST endpoint.
+     */
+    @GetMapping("message/")
+    @ResponseBody
+    public String message() {
+        return "My message"; // just String
+    }
+    
+    /**
+     * Reads the `username` query parameter
+     * and passes it to the template.
+     */
+    @GetMapping("hello/")
+    public String hello(
+        @RequestParam(name="username", required=false, defaultValue="guest") 
+        String str, 
+        Model model
+    ) {
+        
+        model.addAttribute("name", str);
+        return "hello"; // .html can be omitted
+    }
+}
